653e5375e14d303d08052f58ab24ff02e8faeeb9
sunshifeng <shifeng.sun.ac@gmail.com>
2026-03-24 16:27:57 +0800

    增加本地开发模式

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  const isLocal = mode === 'localdev'
 
   return {
     plugins: [react(), tailwindcss()],
@@ -16,12 +17,17 @@ export default defineConfig(({ mode }) => {
     },
     assetsInclude: ['**/*.svg', '**/*.csv'],
     server: {
-      https: {
-        cert: fs.readFileSync('/etc/ssl/cloudflare/sunshifeng.pem'),
-        key: fs.readFileSync('/etc/ssl/cloudflare/sunshifeng.key'),
-      },
-      host: '0.0.0.0',
-      port: 443,
+      ...(isLocal
+        ? { port: 5173 }
+        : {
+            https: {
+              cert: fs.readFileSync('/etc/ssl/cloudflare/sunshifeng.pem'),
+              key: fs.readFileSync('/etc/ssl/cloudflare/sunshifeng.key'),
+            },
+            host: '0.0.0.0',
+            port: 443,
+          }
+      ),
       proxy: {
         '/api': {
           target: env.VITE_API_PROXY_TARGET || 'http://127.0.0.1:8000',
